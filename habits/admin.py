f4570e48cd8ced34c0d9e4d21e4b59f4887c008f
casper <oksanaozturk@gmail.com>
2024-06-20 00:48:14 +0300

    Настроено отображение моделей в Админке

--- habits/admin.py
+++ habits/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from habits.models import Habit
+
+
+@admin.register(Habit)
+class HabitAdmin(admin.ModelAdmin):
+    """Класс для регистрации User в админке."""
+
+    list_display = ("id", "owner", "place", "time", "action", "is_pleasant_habit", "associated_habit",
+                    "periodicity", "bonus", "execution_duration", "is_public")
+    list_filter = ("owner",)
+    search_fields = ("owner", "action")
---
+++ users/urls.py
@@ -0,0 +1,25 @@
+from django.urls import path
+from rest_framework.permissions import AllowAny
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
+from users.apps import UsersConfig
+from users.views import UserListAPIView, UserRetrieveAPIView, UserCreateAPIView, UserUpdateAPIView, UserDestroyAPIView
+
+app_name = UsersConfig.name
+
+urlpatterns = [
+    # Путь для вывода страницы со всеми объектами модели User
+    path("users-list/", UserListAPIView.as_view(), name="users-list"),
+    # Путь для вывода страницы с одним объектом модели User
+    path("user-view/<int:pk>/", UserRetrieveAPIView.as_view(), name="user-retrieve"),
+    # Путь для вывода страницы при создании нового объекта модели User
+    path("user-create/", UserCreateAPIView.as_view(), name="user-create"),
+    # Путь для редактирования объекта модели User
+    path("user-update/<int:pk>/", UserUpdateAPIView.as_view(), name="user-update"),
+    # Путь для удаления объекта модели User
+    path("user-destroy/<int:pk>/", UserDestroyAPIView.as_view(), name="user-destroy"),
+
+    path("login/", TokenObtainPairView.as_view(permission_classes=(AllowAny,)), name='login'),
+    path("token/refresh/", TokenRefreshView.as_view(permission_classes=(AllowAny,)), name='token_refresh'),
+]
+
