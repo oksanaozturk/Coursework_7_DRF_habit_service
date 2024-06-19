9d19c36a78b8d6ea82c0267bb2245b34531f8200
casper <oksanaozturk@gmail.com>
2024-06-20 00:46:31 +0300

    Созданы контроллеры для моделей Habit, User

--- habits/views.py
+++ habits/views.py
@@ -1,3 +1,70 @@
-from django.shortcuts import render
+from rest_framework.generics import CreateAPIView, UpdateAPIView, DestroyAPIView, ListAPIView, RetrieveAPIView
+from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
 
-# Create your views here.
+from habits.models import Habit
+from habits.paginators import CustomPagination
+from habits.serializers import HabitSerializer
+from users.permissions import IsOwner
+
+
+class HabitCreateAPIView(CreateAPIView):
+    """Класс для создания экземпляра модели Habit (CRUD)"""
+    serializer_class = HabitSerializer
+
+    # Необходимо указать IsAuthenticated, так как вносятся изменения на уровне проекта
+    permission_classes = (IsAuthenticated,)
+
+    def perform_create(self, serializer):
+        """Метод для присоединения создателя привычки к Привычке"""
+        habit = serializer.save()
+        habit.owner = self.request.user
+        habit.save()
+
+
+class HabitUpdateAPIView(UpdateAPIView):
+    """Класс для редактирования экземпляра модели Habit (CRUD)"""
+    serializer_class = HabitSerializer
+    # Получаем все данне из БД
+    queryset = Habit.objects.all()
+    permission_classes = (IsOwner,)
+
+
+class HabitDestroyAPIView(DestroyAPIView):
+    """Класс для удаления экземпляра модели Habit (CRUD)"""
+    serializer_class = HabitSerializer
+    queryset = Habit.objects.all()
+    permission_classes = (IsOwner,)
+
+
+class HabitListAPIView(ListAPIView):
+    """Класс для выведения всех экземпляров модели Habit (CRUD)"""
+    serializer_class = HabitSerializer
+    queryset = Habit.objects.all()
+    permission_classes = (IsAuthenticated, IsOwner,)
+    pagination_class = CustomPagination
+
+
+class HabitRetrieveAPIView(RetrieveAPIView):
+    """Класс для выведения одного экземпляра модели Habit (CRUD)"""
+    serializer_class = HabitSerializer
+    queryset = Habit.objects.all()
+    permission_classes = (IsAuthenticated, IsOwner)
+
+
+class PublicHabitListAPIView(ListAPIView):
+    """
+    Класс просмотра списка всех публичных привычек (от всех Пользователей).
+    """
+    serializer_class = HabitSerializer
+    queryset = Habit.objects.all()
+    # pagination_class = HabitCustomPagination
+    # IsAuthenticatedOrReadOnly – только для авторизованных или всем, но для чтения
+    permission_classes = (IsAuthenticatedOrReadOnly,)
+    pagination_class = CustomPagination
+
+    def get_queryset(self):
+        """
+        Переопределение метода,
+        для отображения только публичных привычек.
+        """
+        return Habit.objects.filter(is_public=True)
--- users/views.py
+++ users/views.py
@@ -1,3 +1,50 @@
-from django.shortcuts import render
+from rest_framework.generics import CreateAPIView, UpdateAPIView, DestroyAPIView, ListAPIView, RetrieveAPIView
+from rest_framework.permissions import AllowAny, IsAdminUser, IsAuthenticated
 
-# Create your views here.
+from users.models import User
+from users.serializers import UserSerializer
+
+
+class UserCreateAPIView(CreateAPIView):
+    """Класс для создания экземпляра модели User (CRUD)"""
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+    # Делаем разрешение для этого контроллера, чтобы незарегистрированный пользователь имел доступ к регистрации
+    permission_classes = (AllowAny,)
+
+    def perform_create(self, serializer):
+        """Данный метод необходим для того, чтобы нам не мешала настройка 'username = None',
+        указанная нами в модели User в models.py"""
+        user = serializer.save(is_active=True)
+        # Кешируем обязательно пароль Пользователя
+        user.set_password(user.password)
+        user.save()
+
+
+class UserUpdateAPIView(UpdateAPIView):
+    """Класс для редактирования экземпляра модели User (CRUD)"""
+    serializer_class = UserSerializer
+    # Получаем все данне из БД
+    queryset = User.objects.all()
+    permission_classes = (IsAdminUser,)
+
+
+class UserDestroyAPIView(DestroyAPIView):
+    """Класс для удаления экземпляра модели User (CRUD)"""
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+    permission_classes = (IsAdminUser,)
+
+
+class UserListAPIView(ListAPIView):
+    """Класс для выведения всех экземпляров модели User (CRUD)"""
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+    permission_classes = (IsAuthenticated,)
+
+
+class UserRetrieveAPIView(RetrieveAPIView):
+    """Класс для выведения одного экземпляра модели User (CRUD)"""
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+    permission_classes = (IsAdminUser,)
