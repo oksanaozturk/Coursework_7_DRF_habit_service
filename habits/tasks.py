f16bdda179cd3dbe09bf125da43b4539a0c8a6af
casper <oksanaozturk@gmail.com>
2024-06-22 23:33:47 +0300

    Добавлена периодическая задача

--- habits/tasks.py
+++ habits/tasks.py
@@ -1,43 +1,42 @@
 from celery import shared_task
 from django.utils import timezone
 
+from config import settings
+from habits.models import Habit
+from habits.services import send_tg_message
 
-# тестовая задача проверки celery
+
 @shared_task
-def send_tg_habits_reminder():
-    today = timezone.now().today().date
+def send_tg_habits_message():
+    """
+    Функуия ТГ-рассылки напоминаний о том, в какое время и какие
+    привычки необходимо выполнять, если дата выполнения - сегодня.
+    :return:
+    """
+
+    today = timezone.now().time()     # .today().date отобразится в формате времени
     print(today)
+    habits = Habit.objects.all()     # фильтруем привычки
 
+    for habit in habits:
+        if habit.time <= today:
+            # Формируем сообщение для текущей привычки
+            message = f'Привет! Тебя ждет полезная привычка {habit.action} в {habit.time}, место - {habit.place}'
+            send_tg_message(settings.TELEGRAM_CHAT_ID, message)
 
-# @shared_task
-# def send_tg_habits_reminder():
-#     '''ТГ-рассылка напоминаний о том, в какое время и какие
-#     привычки необходимо выполнять, если дата выполнения - сегодня.'''
-#
-#     today = timezone.now().time()     # .today().date отобразится в формате времени
-#     print(today)
-#     habits = Habit.objects.all()     # фильтруем привычки
-#
-#     for habit in habits:
-#         # Проверяем, что у пользователя есть telegram_id и время выполнения привычки соответствует сегодняшнему дню
-#         if habit.owner.telegram_id:
-#             # Формируем сообщение для текущей привычки
-#             message = f'Привет! Выполни привычку {habit.action} в {habit.time}, место - {habit.location}'
-#             send_tg_message(habit.owner.telegram_id, message)
-#
-#         # Обновление времени выполнения привычки в зависимости от periodicity
-#         if habit.periodicity == 1:
-#             habit.time += today.timedelta(days=1)
-#         elif habit.periodicity == 2:
-#             habit.time += today.timedelta(days=2)
-#         elif habit.periodicity == 3:
-#             habit.time += today.timedelta(days=3)
-#         elif habit.periodicity == 4:
-#             habit.time += today.timedelta(days=4)
-#         elif habit.periodicity == 5:
-#             habit.time += today.timedelta(days=5)
-#         elif habit.periodicity == 6:
-#             habit.time += today.timedelta(days=6)
-#         elif habit.periodicity == 7:
-#             habit.time += today.timedelta(days=7)
-#         habit.save()
+        # Обновление времени выполнения привычки в зависимости от periodicity
+        # if habit.periodicity == 1:
+        #     habit.time += today.timedelta(days=1)
+        # elif habit.periodicity == 2:
+        #     habit.time += today.timedelta(days=2)
+        # elif habit.periodicity == 3:
+        #     habit.time += today.timedelta(days=3)
+        # elif habit.periodicity == 4:
+        #     habit.time += today.timedelta(days=4)
+        # elif habit.periodicity == 5:
+        #     habit.time += today.timedelta(days=5)
+        # elif habit.periodicity == 6:
+        #     habit.time += today.timedelta(days=6)
+        # elif habit.periodicity == 7:
+        #     habit.time += today.timedelta(days=7)
+        # habit.save()
